8f84f6e2786b1f67ecd051b1714658cf85aa93cd
Simon Stone <Simon.Stone@docusign.com>
2022-09-13 09:23:44 +0100

    chore(*): ensure generated code has license headers
    Signed-off-by: Simon Stone <Simon.Stone@docusign.com>

--- AccordProject.Concerto.Tests/scripts/codegen.sh
+++ AccordProject.Concerto.Tests/scripts/codegen.sh
@@ -5,5 +5,5 @@ if [ -d output ]; then
 fi
 npx --yes @accordproject/concerto-cli@unstable parse --model data/patent.cto --output data/patent.json
 npx --yes @accordproject/concerto-cli@unstable compile --model data/employee.cto --target CSharp --strict --useNewtonsoftJson
-mv output/org.accordproject.concerto.test@1.2.3.cs TestTypes.cs
+cat scripts/header.txt output/org.accordproject.concerto.test@1.2.3.cs > TestTypes.cs
 rm -rf output
